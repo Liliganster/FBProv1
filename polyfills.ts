026884d57567244cb88e5bff11e4e13e64b0aae5
Liliganster <liliganster@example.com>
2025-12-08 15:11:09 +0100

    fix: Adjust Promise.withResolvers type assertion and remove ts-ignore.

--- polyfills.ts
+++ polyfills.ts
@@ -1,7 +1,6 @@
 
-if (typeof Promise.withResolvers === 'undefined') {
-    // @ts-ignore
-    Promise.withResolvers = function <T>() {
+if (typeof (Promise as any).withResolvers === 'undefined') {
+    (Promise as any).withResolvers = function <T>() {
         let resolve!: (value: T | PromiseLike<T>) => void;
         let reject!: (reason?: any) => void;
         const promise = new Promise<T>((res, rej) => {
